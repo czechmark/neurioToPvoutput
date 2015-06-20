d88e3cfc99aff5f6cf79a180326852e426453aca
mark <mark@petschek.com>
2015-06-20 09:41:55 -0400

    add in my_keys.py

---
+++ my_keys.py
@@ -0,0 +1,4 @@
+key    = "MyNuerioClientIDGoesHere"
+secret = "MyNuerioClientSecretGoesHere"
+# you will have to determine your Sensor ID using the neurioToCSV python script
+sensor_id = "0x0000CAFEBABE934B"
