d03744356c53c88336dd6e90e7d1a8ccf3c6c654
czechmark <mark@petschek.com>
2015-06-20 09:59:55 -0400

    Update my_keys.py

--- my_keys.py
+++ my_keys.py
@@ -2,3 +2,7 @@ key    = "MyNuerioClientIDGoesHere"
 secret = "MyNuerioClientSecretGoesHere"
 # you will have to determine your Sensor ID using the neurioToCSV python script
 sensor_id = "0x0000CAFEBABE934B"
+location_id = "CAFEBABEQOutKEz2sUBLEg"
+SYSTEMID = "31000"
+APIKEY = "myKey"
+DONATION=True
